48e28bd35d26187d3a0f44742470e55b6d9cd4f4
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-20 18:13:46 +0530

    multi-module project setup for doing the further things

--- pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Vet.java
@@ -1,5 +1,5 @@
 package com.mananluvtocode.pet_clinic.model;
-// It is just like the animal doctor for treating the animal right.
+// It is just like the animal doctor for treating the animals.
 public class Vet extends Person {
 
 }
