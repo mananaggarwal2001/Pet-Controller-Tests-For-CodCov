4ae0eb85f012600fb0fdd5e0c77df822f4f06466
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-25 11:58:26 +0530

    List of all owners is being loaded in the frontend

--- pet-clinic-web/src/main/java/com/mananluvtocode/pet_clinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mananluvtocode/pet_clinic/controllers/OwnerController.java
@@ -1,12 +1,22 @@
 package com.mananluvtocode.pet_clinic.controllers;
 
+import org.springframework.ui.Model;
+import com.mananluvtocode.pet_clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
+    // declaring final because the object will never be change for doing the further work.
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html", ""})
-    public String ownerpage() {
+    public String ownerpage(Model themodel) {
+        themodel.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
