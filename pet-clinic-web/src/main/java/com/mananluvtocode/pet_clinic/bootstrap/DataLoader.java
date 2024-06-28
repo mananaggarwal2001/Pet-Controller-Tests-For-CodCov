d7400c9d07bb050d674f99b04da5d035aa69a510
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-28 11:00:42 +0530

    saved the data on load for pettype for doing the further work.

--- pet-clinic-web/src/main/java/com/mananluvtocode/pet_clinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/mananluvtocode/pet_clinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.mananluvtocode.pet_clinic.bootstrap;
 
 import com.mananluvtocode.pet_clinic.model.Owner;
+import com.mananluvtocode.pet_clinic.model.PetType;
 import com.mananluvtocode.pet_clinic.model.Vet;
 import com.mananluvtocode.pet_clinic.services.OwnerService;
+import com.mananluvtocode.pet_clinic.services.PetTypeService;
 import com.mananluvtocode.pet_clinic.services.VetService;
 import com.mananluvtocode.pet_clinic.services.map.OwnerServiceMap;
 import com.mananluvtocode.pet_clinic.services.map.VetServiceMap;
@@ -10,10 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+// this implements the data of the class on load for doing the work.
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petType;
 
     // hard code implementation which is
 //    public DataLoader(){
@@ -23,13 +27,23 @@ public class DataLoader implements CommandLineRunner {
 
     // this below constructor the classes are initialized by the Spring itself.
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petType) {
         this.ownerService = ownerService; // the Spring will initialize this annotation for doing the work.
         this.vetService = vetService;
+        this.petType = petType;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petType.save(dog);
+
+
+        PetType  cat= new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petType.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("John");
         owner1.setLastName("Doe");
