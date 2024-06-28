0ac3a9f8c998543a9122766f8ca91b0475474d68
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-28 10:14:55 +0530

    petType Services for doing the further work.

---
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.mananluvtocode.pet_clinic.services;
+
+import com.mananluvtocode.pet_clinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+    // this will remain empty for doing the further work.
+}
