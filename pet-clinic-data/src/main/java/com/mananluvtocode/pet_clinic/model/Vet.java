76051b3699cc9ecbc9ec83eebc9a8063abc73b8e
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-28 09:56:01 +0530

    created the speciality POJO Class and then we do the further work.

--- pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Vet.java
@@ -1,5 +1,16 @@
 package com.mananluvtocode.pet_clinic.model;
+
+import java.util.Set;
+
 // It is just like the animal doctor for treating the animals.
 public class Vet extends Person {
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
