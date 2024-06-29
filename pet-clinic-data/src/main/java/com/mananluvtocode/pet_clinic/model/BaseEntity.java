3e06e26a07993a0f0261ee77337db15dcdeed651
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-29 17:23:09 +0530

    Implementing the base entity

--- pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/BaseEntity.java
@@ -1,10 +1,20 @@
 package com.mananluvtocode.pet_clinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
+@MappedSuperclass
+// this is telling that this is being inherited by the other class and this class is not the part of the database entity.
 public class BaseEntity implements Serializable {
     // box types can be null in fact the primitive values can't be null for doing the things.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     public Long getId() {
         return id;
     }
