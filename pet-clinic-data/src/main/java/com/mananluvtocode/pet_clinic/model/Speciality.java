51f3e69ed05242423bffd93780e18dc0bb84c47c
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-30 15:16:04 +0530

    Converted the Speciality and vet into the Entity

--- pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Speciality.java
@@ -1,6 +1,16 @@
 package com.mananluvtocode.pet_clinic.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Cleanup;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/Vet.java
@@ -1,10 +1,18 @@
 package com.mananluvtocode.pet_clinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 // It is just like the animal doctor for treating the animals.
+
+@Entity // this tells the JPA that this table is be formed in the database for storing the data.
+@Table(name = "vets")
 public class Vet extends Person {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
