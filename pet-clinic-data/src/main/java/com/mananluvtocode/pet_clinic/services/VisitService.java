8a08c9ecb06a040949fe50c38d5eb6cee3cc2be6
AnonymousManan <mananaggarwal2001@gmail.com>
2024-07-04 14:40:37 +0530

    changes made in the visit Service.

--- pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/services/VisitService.java
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/services/VisitService.java
@@ -3,4 +3,5 @@ package com.mananluvtocode.pet_clinic.services;
 import com.mananluvtocode.pet_clinic.model.Visit;
 
 public interface VisitService extends CrudService<Visit, Long> {
+    // this repository will be empty always for doing the further work.
 }
