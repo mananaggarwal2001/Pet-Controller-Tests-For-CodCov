dacb598f6c45a7ce3d6252e02449d39db2a77d8d
AnonymousManan <mananaggarwal2001@gmail.com>
2024-07-08 08:28:03 +0530

    Initiated the web data binder for not allowing the manipulation of the id in the Spring boot

--- pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mananluvtocode/pet_clinic/model/PetType.java
@@ -1,6 +1,5 @@
 package com.mananluvtocode.pet_clinic.model;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
--- pet-clinic-web/src/main/java/com/mananluvtocode/pet_clinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mananluvtocode/pet_clinic/controllers/OwnerController.java
@@ -4,7 +4,9 @@ import com.mananluvtocode.pet_clinic.model.Owner;
 import org.springframework.ui.Model;
 import com.mananluvtocode.pet_clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +19,15 @@ public class OwnerController {
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    // to control the binding of the data into the objects we use the webDataBinder feature for doing this.
+    // This means that the we do not want to allow the webforms to address and manipulate the ID that means everything to the database for doing the stuffs.
+    // This also means that in every request spring will not allow the manipulation of the I'D when processing the request for doing the stuffs.
+    //InitBinder is the Standard Spring MVC controller for injecting things into the controller.
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"/", "/index", "index.html", ""})
